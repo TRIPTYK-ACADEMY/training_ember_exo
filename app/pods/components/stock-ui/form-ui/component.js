b290f48ecd425604c7a11748c88a350e40f577e5
remadex <s.cardon@outlook.com>
2021-06-14 09:40:34 +0200

    fix a small bug

--- app/pods/components/stock-ui/form-ui/component.js
+++ app/pods/components/stock-ui/form-ui/component.js
@@ -1,7 +1,7 @@
-import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default class StockUiFormUiComponent extends Component {
   @service router;
@@ -12,10 +12,10 @@ export default class StockUiFormUiComponent extends Component {
   @tracked price;
 
   constructor(owner, args) {
-    super(owner, args)
-    this.title = '';
-    this.description = '';
-    this.img = '';
+    super(owner, args);
+    this.title = "";
+    this.description = "";
+    this.img = "";
     this.price = null;
   }
 
@@ -24,24 +24,23 @@ export default class StockUiFormUiComponent extends Component {
     e.preventDefault();
     this.args.wine.description = this.description;
     this.args.wine.title = this.title;
-    this.args.wine.img = this.img
+    this.args.wine.img = this.img;
     this.args.wine.price = parseInt(this.price) || 0;
-    this.args.wine.save()
-    this.router.transitionTo('wines');
+    this.args.wine.save();
+    this.router.transitionTo("wines");
   }
 
   @action
   reset(e) {
     e.target.reset();
-    this.router.transitionTo('wines');
+    this.router.transitionTo("wines");
   }
 
   @action
   update() {
     this.description = this.args.wine.description;
-    this.title = this.args.wine.title
-    this.img = this.args.wine.img
-    this.price = this.args.wine.price
+    this.title = this.args.wine.title;
+    this.img = this.args.wine.img;
+    this.price = this.args.wine.price;
   }
-
 }
